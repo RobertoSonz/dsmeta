35c4f79d8559b0ddd8c99e389b0b4524bcaa7ea4
Roberto Sonza <robertsonzalfa@gmail.com>
2022-09-09 09:16:37 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -4,6 +4,8 @@ import './styles.css';
 import NotificationButton from "../NotificationButton";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { BASE_URL } from "../../utils/request";
+import { Sale } from "../../models/sale";
 function SalesCard() {
 
     const min = new Date(new Date().setDate(new Date().getDate() -365));
@@ -12,10 +14,12 @@ function SalesCard() {
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    const [sales, setSales] = useState<Sale[]>([]);
+
     useEffect(() => {
-       axios.get("http://localhost:8080/sales")
+       axios.get(`${BASE_URL}/sales`)
        .then(response => {
-        console.log(response.data);
+         setSales(response.data.content);
        })
     }, []);
 
@@ -55,240 +59,23 @@ function SalesCard() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="show992">#120</td>
-                            <td className="show576">01/08/2022</td>
-                            <td>Ana</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
+                        {sales.map(sale => {
+                        return(
+                            <tr key={sale.id}>
+                            <td className="show992">{sale.id}</td>
+                            <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                            <td>{sale.sellerName}</td>
+                            <td className="show992">{sale.visited}</td>
+                            <td className="show992">{sale.deals}</td>
+                            <td>R$ {sale.amount.toFixed(2)}</td>
                             <td>
                                 <div className="dsmeta-red-btn-container">
                                     <NotificationButton />
                                 </div>
                             </td>
                         </tr>
-                        <tr>
-                            <td className="show992">#121</td>
-                            <td className="show576">04/08/2022</td>
-                            <td>Carlos</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 24300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#122</td>
-                            <td className="show576">07/08/2022</td>
-                            <td>Pedro</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 45500.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#123</td>
-                            <td className="show576">10/08/2022</td>
-                            <td>João</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 5600.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#124</td>
-                            <td className="show576">20/08/2022</td>
-                            <td>Chico</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 22700.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#125</td>
-                            <td className="show576">25/08/2022</td>
-                            <td>Gilberto</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 4500.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#126</td>
-                            <td className="show576">01/09/2022</td>
-                            <td>Maria</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 16300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#127</td>
-                            <td className="show576">03/09/2022</td>
-                            <td>Jussara</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 2300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#128</td>
-                            <td className="show576">05/09/2022</td>
-                            <td>Leandro</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 25300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#129</td>
-                            <td className="show576">10/09/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 58300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#130</td>
-                            <td className="show576">15/09/2022</td>
-                            <td>Severino</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 16300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#131</td>
-                            <td className="show576">20/09/2022</td>
-                            <td>Pedro</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 4300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#132</td>
-                            <td className="show576">21/09/2022</td>
-                            <td>Paulo</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 15300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#133</td>
-                            <td className="show576">25/09/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 7300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#134</td>
-                            <td className="show576">27/09/2022</td>
-                            <td>Leo</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 50300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#135</td>
-                            <td className="show576">01/10/2022</td>
-                            <td>Leonardo</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 5300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#136</td>
-                            <td className="show576">05/10/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 7300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#137</td>
-                            <td className="show576">07/10/2022</td>
-                            <td>Pedro</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 5500.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
+                        )
+                        })}
                     </tbody>
                 </table>
             </div>
